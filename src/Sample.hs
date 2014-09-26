cd25fad3f474f8842e00a57648fb3526e585f6bc
Ryan Mulligan <ryan@ryantm.com>
2014-09-25 20:06:04 -0700

    presentation 4 - separation of concerns

--- src/Sample.hs
+++ src/Sample.hs
@@ -1,1 +1,29 @@
 module Sample where
+
+type Lit = Bool
+
+data Lamp = O | R | Y
+          deriving (Eq, Show)
+
+berlinLight :: (Int -> Int -> Int) -> Int -> [Lit]
+berlinLight partitioner n = (litLamps ++ (repeat False))
+  where litLamps = take (n `partitioner` 5) (repeat True)
+
+colorFilter :: [Lamp] -> [Lit] -> [Lamp]
+colorFilter colors lampLits =
+  map colorMe $ zip (cycle colors) lampLits
+  where
+    colorMe (color, True) = color 
+    colorMe (color, False) = O
+
+berlinHourFives :: Int -> [Lamp]
+berlinHourFives h = take 4 $ colorFilter [R] $ berlinLight div h
+
+berlinHourOnes :: Int -> [Lamp]
+berlinHourOnes h = take 4 $ colorFilter [R] $ berlinLight rem h
+
+berlinMinuteOnes :: Int -> [Lamp]
+berlinMinuteOnes m = take 4 $ colorFilter [Y] $ berlinLight rem m
+
+berlinMinuteFives :: Int -> [Lamp]
+berlinMinuteFives m = take 11 $ colorFilter [Y,Y,R] $ berlinLight div m
--- test/SampleSpec.hs
+++ test/SampleSpec.hs
@@ -7,10 +7,37 @@ import Sample
 
 spec :: Spec
 spec = do
-  describe "matchers" $ do
-    it "works" $
-      True `shouldBe` True
+  describe "berlinHourFives" $ do
+    it "returns lamps when given hours" $ do
+      berlinHourFives 0 `shouldBe` [O,O,O,O]
+      berlinHourFives 1 `shouldBe` [O,O,O,O]
+      berlinHourFives 2 `shouldBe` [O,O,O,O]
+      berlinHourFives 5 `shouldBe` [R,O,O,O]
+      berlinHourFives 6 `shouldBe` [R,O,O,O]
+      berlinHourFives 10 `shouldBe` [R,R,O,O]
+      berlinHourFives 15 `shouldBe` [R,R,R,O]
+      berlinHourFives 20 `shouldBe` [R,R,R,R]
+  describe "berlinHourOnes" $ do
+    it "returns lamps when given hours" $ do
+      berlinHourOnes 0 `shouldBe` [O,O,O,O]
+      berlinHourOnes 1 `shouldBe` [R,O,O,O]
+      berlinHourOnes 2 `shouldBe` [R,R,O,O]
+      berlinHourOnes 3 `shouldBe` [R,R,R,O]
+      berlinHourOnes 4 `shouldBe` [R,R,R,R]
+      berlinHourOnes 5 `shouldBe` [O,O,O,O]
+      berlinHourOnes 6 `shouldBe` [R,O,O,O]
+      berlinHourOnes 20 `shouldBe` [O,O,O,O]
+  describe "berlinMinuteOnes" $ do
+    it "returns lamps when given minutes" $ do
+      berlinMinuteOnes 0 `shouldBe` [O,O,O,O]
+      berlinMinuteOnes 1 `shouldBe` [Y,O,O,O]
+      berlinMinuteOnes 2 `shouldBe` [Y,Y,O,O]
+  describe "berlinMinuteFives" $ do
+    it "returns lamps when given minutes" $ do
+      berlinMinuteFives 0 `shouldBe` [O,O,O,O,O,O,O,O,O,O,O]
+      berlinMinuteFives 1 `shouldBe` [O,O,O,O,O,O,O,O,O,O,O]
+      berlinMinuteFives 5 `shouldBe` [Y,O,O,O,O,O,O,O,O,O,O]
+      berlinMinuteFives 6 `shouldBe` [Y,O,O,O,O,O,O,O,O,O,O]
+      berlinMinuteFives 10 `shouldBe` [Y,Y,O,O,O,O,O,O,O,O,O]
+      berlinMinuteFives 15 `shouldBe` [Y,Y,R,O,O,O,O,O,O,O,O]
 
-  describe "props" $ do
-    prop "works" $
-      \ x -> id x == (x :: Int)
